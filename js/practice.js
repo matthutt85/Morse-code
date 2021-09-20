b584168eda736661422290eccb15712a4123819b
matthutt85 <matt.hutt85@gmail.com>
2021-09-20 19:06:36 +0100

    css needs work, function needs finishing?

--- js/practice.js
+++ js/practice.js
@@ -1,31 +1,34 @@
 
 
     
-    const alphabets = {
+     const letters = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];    
     
-        letters: [
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],    
-    
-        morse: [
-        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",]
-        
-    }    
+     const morse =  [
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",];
     
  
 
     
-// const letterInput = document.querySelector(".translator__input");
-// const morseOutput = document.querySelector(".translator__output");
+const inputValue = document.getElementById(".translator__input");
+const outputValue = document.getElementById(".translator__output");
 
 
-// console.log(alphabets);
-
-const lettersToMorse = (word) => {
-    for (let i = 0; i < word.length; i++) {
-        if ()
+// console.log(letters);
+const lettersToMorse = (English) => {
+    let indexMatch;
+    for (let i = 0; i < letters.length; i++) {
+        if (letters[i] == English) {
+            indexMatch = i
+        }
     }
+    return morse[indexMatch];
 }
 
+// letters.forEach((letter) => {
+//     console.log(letter);
+// })
+console.log(lettersToMorse("b"));
 
 
 
--- js/translator.js
+++ js/translator.js
@@ -2,9 +2,20 @@ import { letters, morse } from "../data/data.js"
 
 
 
-const inputValue = document.getElementById("translator__input");
-const outputValue = document.getElementById("translator__output");
+// const inputValue = document.getElementById("translator__input");
+// const outputValue = document.getElementById("translator__output");
+
+// //how to link input value to this function???
 
 
+// const lettersToMorse = (English) => {
+//     let indexMatch; // basically saying "a" = [0]
+//     for (let i = 0; i < letters.length; i++) {
+//         if (letters[i] == English) {
+//             indexMatch = i
+//         }
+//     }
+//     return morse[indexMatch];
+// }
 
-
+// const 
